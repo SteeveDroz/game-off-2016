07bde2defd0b983c880cf5dab46d71ce18939e8c
Steeve Droz <steeve.droz@sudest-webdesign.com>
2016-11-07 11:58:01 +0100

    Procedurally looking for a class

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
 
-import active.*;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private String[] scenes;
+
+    public Main() {
+	scenes = new String[] { "Welcome" };
+    }
+
     @Override
     public void start(Stage primaryStage) {
 	try {
-	    Welcome scene = new Welcome();
+	    Scene scene = null;
+	    for (String sceneName : scenes) {
+		try {
+		    Class<?> sceneClass = Class.forName("active." + sceneName);
+		    scene = (Scene) sceneClass.newInstance();
+		    break;
+		} catch (ClassNotFoundException exception) {
+		    continue;
+		}
+	    }
 	    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	    primaryStage.setScene(scene);
 	    primaryStage.setTitle("GitHub Game Off 2016 (WORKING TITLE)");
 	    primaryStage.show();
 	} catch (Exception e) {
-	    e.printStackTrace();
+	    Alert alert = new Alert(AlertType.ERROR);
+	    alert.setTitle("An error occurred");
+	    alert.setHeaderText("An error occurred");
+	    alert.setContentText("The game is being reinitialized.\nPlease run again to play.");
+	    alert.show();
+	    // TODO Reinitialize
 	}
     }
 
