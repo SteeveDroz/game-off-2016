dd5130af76bc34f44c708ffc4d836b762348584f
Steeve Droz <steeve.droz@sudest-webdesign.com>
2016-11-11 09:59:27 +0100

    Reinit is made from backups

--- src/core/com/github/steevedroz/powercycle/Main.java
+++ src/core/com/github/steevedroz/powercycle/Main.java
@@ -84,11 +84,16 @@ public class Main extends Application {
     }
 
     private static void reinit() {
-	for (String activeClass : ACTIVE_CLASSES) {
-	    activate(activeClass);
-	}
-	for (String inactiveClass : INACTIVE_CLASSES) {
-	    deactivate(inactiveClass);
+	new File(MAIN_DIRECTORY + INACTIVE_FOLDER).mkdirs();
+	Path backupActive = Paths.get(BACKUP_DIRECTORY, ACTIVE_FOLDER);
+	Path backupInactive = Paths.get(BACKUP_DIRECTORY, INACTIVE_FOLDER);
+	Path active = Paths.get(MAIN_DIRECTORY, ACTIVE_FOLDER);
+	Path inactive = Paths.get(MAIN_DIRECTORY, INACTIVE_FOLDER);
+	try {
+	    copy(backupActive, active);
+	    copy(backupInactive, inactive);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
     }
 
@@ -109,13 +114,22 @@ public class Main extends Application {
         }
     }
 
-    private static void activate(String name) {
-	File file = new File(MAIN_DIRECTORY + INACTIVE_FOLDER + name + ".class");
-	file.renameTo(new File(MAIN_DIRECTORY + ACTIVE_FOLDER + name + ".class"));
+    private static void tryToMove(File file, String[] classList, String destination) throws FinishedMovingException {
+        for (String className : classList) {
+            if (file.getName().equals(className + ".class")) {
+        	file.renameTo(new File(destination + file.getName()));
+        	throw new FinishedMovingException();
+            }
+        }
     }
 
-    private static void deactivate(String name) {
-	File file = new File(MAIN_DIRECTORY + ACTIVE_FOLDER + name + ".class");
-	file.renameTo(new File(MAIN_DIRECTORY + INACTIVE_FOLDER + name + ".class"));
+    private static void copy(Path from, Path to) throws IOException {
+	Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
+	    @Override
+	    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		Files.copy(file, to.resolve(from.relativize(file)), StandardCopyOption.REPLACE_EXISTING);
+		return FileVisitResult.CONTINUE;
+	    }
+	});
     }
 }
